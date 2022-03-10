78dcd7dcfc674a95d10be0ed605f25e2b9fa703e
Данила Корж <danila.schults@yandex.ru>
2022-03-10 17:30:53 +0300

    HTML CSS v.2 + JS animation

---
+++ JS/input-animate.js
@@ -0,0 +1,22 @@
+let frm = document.querySelectorAll('.inpt');
+let arrFormText = Array.from(frm);
+let formMethod = arrFormText.map(el =>{
+    el.addEventListener('focus', inpFoc =>{
+      //  el.parentElement.children(0).style.color = 'red';
+        el.parentElement.children[0].style.top = '-40px';
+        el.parentElement.children[0].style.fontWeight = '400';
+        el.parentElement.children[0].style.letterSpacing = '1px';
+        el.parentElement.children[0].style.left = '5px';
+        el.parentElement.children[0].style.color = '#d8d8d8';
+    })
+    el.addEventListener('blur', inpBlur =>{
+        //  el.parentElement.children(0).style.color = 'red';
+        if(el.value == false){
+          el.parentElement.children[0].style.top = '-7px';
+          el.parentElement.children[0].style.left = '12px';
+          el.parentElement.children[0].style.fontWeight = '400';
+          el.parentElement.children[0].style.letterSpacing = '0px';
+          el.parentElement.children[0].style.color = '#929292'
+        }
+      })
+})
